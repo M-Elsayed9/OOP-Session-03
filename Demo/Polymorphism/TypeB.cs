26aad8d3ebcf331aa5a592b6ce8dc6ccdce4cca6
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-08-01 02:33:42 +0300

    Polymorphism

---
+++ Demo/Polymorphism/TypeB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Polymorphism
+{
+    internal class TypeB : TypeA
+    {
+        public int B { get; set; }
+
+        public TypeB(int a, int b)
+            : base(a)
+        {
+            B = b;
+        }
+
+        // overriding
+        // 1. using new keyword
+        // 2. using override keyword
+
+        //apply function overriding using new keyword
+
+        public new void MyFun01() // new version 
+        {
+            Console.WriteLine("i am derived class [child]");
+        }
+    
+        // apply function overriding using override keyword
+
+        public override void MyFun02() // override version
+        {
+            Console.WriteLine($"TypeB : A = {A}, B = {B}");
+        }
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,83 @@
-﻿namespace Demo
+﻿using Demo.Polymorphism;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Demo
 {
     internal class Program
     {
+        #region Polymorhism (Overloading)
+        // Method Overloading
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static double Sum(double a, double b)
+        {
+            return (a + b);
+        }
+
+        public static int Sum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public static double Sum(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+
+        public static double Sum(double a, int b)
+        {
+            return a + b;
+        }
+
+        public static double Sum(int a, double b)
+        {
+            return a + b;
+        }
+
+        //public static double Sum(int b, double a) invalid because the signature already exists
+        //            {
+        //    return a + b;
+        //}
+
+
+        #endregion
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Polymorhism (Overloading)
+
+            int result1 = Sum(10, 20);
+            Console.WriteLine(result1);
+            int result2 = Sum(10, 20, 30);
+
+
+
+
+            #endregion
+
+            #region Polymorhism (Overriding)
+            // supported within classes only
+            // function overriding must be with relationship inheritance
+            // function overriding must be with virtual keyword in the base class
+            // function overriding must be with override keyword in the derived class
+
+            TypeA typeA = new TypeA(10);
+            typeA.A = 100;
+            typeA.MyFun01();
+            typeA.MyFun02();
+
+            TypeB typeB = new TypeB(10, 20);
+            typeB.A = 100;
+            typeB.B = 200;
+            typeB.MyFun01();
+            typeB.MyFun02();
+            #endregion
+
+
         }
     }
 }
